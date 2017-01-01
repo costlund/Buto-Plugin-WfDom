460d69e05ad2d6561486e351eb6951e7c8bc9653
costlund <claes@webforms.se>
2017-01-01 17:32:45 +0100

    i18n support.

--- public/dom.js
+++ public/dom.js
@@ -1,3 +1,27 @@
+function plugin_wf_i18n(){
+  this.data = [];
+  this.translate = function(key){
+    for(var i=0; i<PluginWfI18n.data.length; i++){
+      if(PluginWfI18n.data[i].key==key){
+        var value = PluginWfI18n.data[i].value;
+        if(PluginWfI18n.data[i].replace){
+          var replace = PluginWfI18n.data[i].replace;
+          for(var j=0; j<replace.length; j++){
+            value = value.replace(replace[j].set, replace[j].to);
+          }
+        }
+        return value;
+      }
+    }
+    return key;
+  }
+}
+var PluginWfI18n = new plugin_wf_i18n();
+//PluginWfI18n.data.push({key: 'Close', value: 'stäng', replace: [{set: 's', to: 'S'}]});
+//PluginWfI18n.data.push({key: 'Create account', value: 'Skapa konto'});
+//if(typeof(PluginWfI18n)==='object'){
+//  console.log(PluginWfI18n.translate(('Close')));
+//}
 function plugin_wf_dom(){
   /**
    * Pas an object like [{type: 'div', innerHTML: 'Append div in body.'}] and optional parent to create elements. Nestle other objects in innerHTML to create multiple elements once.
@@ -19,7 +43,20 @@ function plugin_wf_dom(){
       parent.appendChild(obj);
       if(element[i].innerHTML){
         if((typeof element[i].innerHTML) === 'string'){
-          obj.innerHTML = element[i].innerHTML;
+          /**
+           * Check if i18n is availible.
+           */
+          if(typeof(PluginWfI18n.data)==='object' && typeof(PluginWfI18n.translate)==='function'){
+            /**
+             * i18n is availible.
+             */
+            obj.innerHTML = PluginWfI18n.translate(element[i].innerHTML);
+          }else{
+            /**
+             * i18n is NOT availible.
+             */
+            obj.innerHTML = element[i].innerHTML;
+          }
         }else if((typeof element[i].innerHTML) === 'object') {
           this.render(element[i].innerHTML, obj);
         }
@@ -32,9 +69,6 @@ function plugin_wf_dom(){
       element = document.getElementById(element);
     }
     element.parentNode.removeChild(element);
-    
   }
 }
 var PluginWfDom = new plugin_wf_dom()
-
-
